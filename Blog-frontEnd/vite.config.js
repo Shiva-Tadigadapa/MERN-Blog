3ef660a39aa196332659250f14f724573f87faad
shiva <shivatadigadapa@gmail.com>
2023-07-10 12:17:20 +0530

    /dc

--- Blog-frontEnd/vite.config.js
+++ Blog-frontEnd/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react', 'react-lottie'],
+      external: ['react-lottie'],
     },
   },
 });
